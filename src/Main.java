4c91cbfcbccf243c88df7477bb0989f70a7ccd8c
Zan <stankovic.zan@gmail.com>
2023-07-27 18:32:27 +0200

    Malo bolj dovrsena stvar, ampak sprogramirano lose

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,56 @@ public class Main {
         System.out.println();
         System.out.println("1. Celzije --> Fahrenheite");
         System.out.println("2. Fahrenheite --> Celzije");
+        System.out.println("-----------------------------");
         int cifra = sc.nextInt();
-        System.out.println("Vpisite temperaturo...");
+        //tuki zgori zdej dobimo v kaj hoemo pretvorit
+
+
+        // in funkcija vrne temperaturo, zdej jo mormo pa se pretvorit
+
+
+        System.out.println("-----------------------------");
 
         Scanner scanner = new Scanner(System.in);
-        float temperatura = scanner.nextFloat();
-        double temperaturaa = (double) temperatura;
+        if(cifra == 1){
+            //izpisemo napisite stopinje celzije da pretvorim v fahrenheite
+            System.out.println("Napisite °C da pretvorimo v °F");
 
-        if(cifra == 1){//tuki imamo celzije in mormo v fahrenheite
-            Pretvorba pretvorba = new Pretvorba(temperaturaa,0);
-            System.out.println(pretvorba.vFahrenheite());
         }
-        else if (cifra == 2){//tuki imamo fahrenheite in mormo v celzije
-            Pretvorba pretvorba = new Pretvorba(0, temperaturaa);
-            System.out.println(pretvorba.vCelzije());
+        else if (cifra == 2) {
+            //izpisemo napisite fahrneheite da pretvorim v celzije
+            System.out.println("Napisite °F da pretvorimo v °C");
 
         }
         else{
             System.out.println("Nekaj je slo narobe");
         }
+
+        Scanner scannner = new Scanner(System.in);
+
+        double temperatura = scannner.nextDouble();
+        //tuki mamo temperaturo
+
+        System.out.println("-----------------------------");
+        if (cifra == 1){
+            System.out.println(temperatura + "°C = " + Pretvorba.CelzijavFahrenheite(temperatura) + "°F");
+        } else if (cifra == 2) {
+            System.out.println(temperatura + "°F = " +  Pretvorba.FahrenheitvCelzije(temperatura) + "°C ");
+        }
+        else{
+            System.out.println("Nekje je bila napaka");
+        }
+
+
+
+
+
+
+
+
     }
+
+
+
+
 }
--- src/Pretvorba.java
+++ src/Pretvorba.java
@@ -1,41 +1,18 @@
 public class Pretvorba {
-    private double celzije;
-    private double fahrenheit;
 
-    public Pretvorba(double celzije, double fahrenheit) {
-        this.celzije = celzije;
-        this.fahrenheit = fahrenheit;
-    }
-
-    public double getCelzije() {
-        return celzije;
-    }
-
-    public void setCelzije(float celzije) {
-        this.celzije = celzije;
-    }
-
-    public double getFahrenheit() {
-        return fahrenheit;
-    }
-
-    public void setFahrenheit(float fahrenheit) {
-        this.fahrenheit = fahrenheit;
-    }
-
-    public double vFahrenheite(){
-        double izracun = (this.celzije * 1.8) + 32;
+    public static double CelzijavFahrenheite(double celzije){
+        double izracun = (celzije * 1.8) + 32;
         return izracun;
     }
 
-    public double vCelzije(){
-        double izracun = (this.fahrenheit - 32) / 1.8;
+    public static double FahrenheitvCelzije(double fahrenheit){
+        double izracun = (fahrenheit - 32) / 1.8;
         return izracun;
     }
 
-    //naredu bom class pretvorba ki bo imel dva fields
-    // celzije in fahremheit
-    // in v mainu odvisno kaj bo uporabnik napisu
-    // te podatki bodo sli v konstruktor
-    // in uporabli funkcijo in je to to
 }
+
+
+
+
+
